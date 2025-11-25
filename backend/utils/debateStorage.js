7f26dca7b91d0b42cb1da8e06cd89dafb21dff3d
ayush-sreejith <ayushsreejith06@gmail.com>
2025-11-25 15:04:54 -0700

    feat(backend): add saveDebate helper to debateStorage

--- backend/utils/debateStorage.js
+++ backend/utils/debateStorage.js
@@ -41,6 +41,21 @@ async function findDebateById(id) {
   return debates.find(d => d.id === id) || null;
 }
 
+async function saveDebate(debate) {
+  const debates = await loadDebates();
+  const idx = debates.findIndex(d => d.id === debate.id);
+
+  const data = debate.toJSON ? debate.toJSON() : debate;
+
+  if (idx >= 0) {
+    debates[idx] = data;
+  } else {
+    debates.push(data);
+  }
+
+  await saveDebates(debates);
+}
+
 module.exports = {
   loadDebates,
   saveDebates
