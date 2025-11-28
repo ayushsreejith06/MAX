4711abd01071cae53d21687c1252fe9ced4698d9
ayush-sreejith <ayushsreejith06@gmail.com>
2025-11-28 11:27:18 -0700

    fix: convert hardhat.config to ESM-compatible format

---
+++ contracts/hardhat.config.mjs
@@ -0,0 +1,12 @@
+import "@nomicfoundation/hardhat-toolbox";
+
+/** @type import("hardhat/config").HardhatUserConfig */
+const config = {
+  solidity: "0.8.20",
+  networks: {
+    hardhat: {}
+  }
+};
+
+export default config;
+
